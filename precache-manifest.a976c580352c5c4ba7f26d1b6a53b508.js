a3f5044ccac195178d842e832b9ee34243dab215
coolPreye <kerebipreye@gmail.com>
2019-11-12 15:23:52 -0800

    Updates

---
+++ precache-manifest.a976c580352c5c4ba7f26d1b6a53b508.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff889b667141de1b772517e3642b9b3c",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "cc7cf502a500dbf64bed",
+    "url": "/my-react-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7c61d4fb0e8fd4c7127e",
+    "url": "/my-react-app/static/js/2.fcfcd4b9.chunk.js"
+  },
+  {
+    "revision": "cc7cf502a500dbf64bed",
+    "url": "/my-react-app/static/js/main.c72cbb81.chunk.js"
+  },
+  {
+    "revision": "91773c727638f51e2f37",
+    "url": "/my-react-app/static/js/runtime-main.e7834099.js"
+  }
+]);
